e2e368708ab263ef313ba9f85d862e4e74a27b5e
810807 <810807@SAIT225654IT.ACDM.DS.SAIT.CA>
2021-03-21 23:04:39 -0600

    week 9 lab

---
+++ src/java/dataaccess/DBUtil.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dataaccess;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class DBUtil {
+    public static final EntityManagerFactory EMF = Persistence.createEntityManagerFactory("UserPU");
+    
+    public static EntityManagerFactory getEmFactory() {
+        return EMF;
+    }
+}
